722679536174f97f12946d145b9d67846de3e692
Atharv Deshmukh <atharvdeshmukh@Atharvs-MacBook-Air.local>
2023-08-22 01:23:00 -0700

    upadated some content and commented out logos for soorya to work on

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,6 @@ const montserrat = Montserrat({ subsets: ['latin'], weight: '800' })
 export default function Home() {
   return (
     <div className={hankenGrotesk.className}>
-      <Head>
-        <meta property="og:image" content="<generated>" />
-        <meta property="og:image:type" content="<generated>" />
-        <meta property="og:image:width" content="<generated>" />
-        <meta property="og:image:height" content="<generated>" />
-      </Head>
       <section id="landing" className="landing flex h-[100vh] min-h-[800px] relative justify-center items-center">
         <div className="content">
           <ul className='text-center flex flex-col justify-center items-center'>
@@ -42,17 +36,25 @@ export default function Home() {
               <h1 className="landing-text flex m-auto font-waterfall text-7xl lg:text-8xl xl:text-9xl text-white font-black md:max-w-[80vw] max-w-[90vw] drop-shadow-[0_10px_0_#4824b3]">HELP US MAKE IT HAPPEN.</h1>
             </div>
             <a href="/#tiers">
-              <button className="bg-audiofeudpurpledark text-xl sm:text-2xl md:text-3xl font-normal text-white font-bold px-[50px] py-6 my-[40px] rounded-full" type="submit">Send us an email</button>
+              <button className="bg-audiofeudpurpledark text-xl sm:text-2xl md:text-3xl font-normal text-white font-bold px-[50px] py-6 my-[40px] rounded-full" type="submit">Learn More</button>
             </a>
           </ul>
         </div>
       </section>
       <section className="about min-h-[800px] h-fit">
         <h1 className="pt-12 text-center text-audiofeudpurple font-extrabold font-waterfall lg:text-6xl md:text-5xl sm:text-5xl text-4xl">Our Mission</h1>
-        <div className="flex justify-center">
-          <p className="text-center max-w-4xl text-audiofeudpurple md:text-xl sm:text-large text-medium px-4 py-4">For better or for worse, garage jam sessions after school are a thing of the past. As of 2023, more kids than ever are making music on their laptops instead, owing to the massively expanded creative ability designing music literally from the sound wave up introduces. Unfortunately, the vast majority of these bedroom musicians operate alone, lack access to quality equipment, and choose not to share their work. This represents an irrecoverable loss of ideas, creative potential, and dreams. Our goal as an organization is to take a flamethrower to these issues and build community, confidence, and lasting opportunities for the next generation of musicians.<br/><br/>So, we’re launching an overnight in-person event in which high schoolers and eligible college students will network, collaborate, and compete to create an original song from scratch. Artists will present before their peers and a set of qualified judges for the chance to win money and essential sound engineering equipment. The aim of this format is to draw out hidden talent and essentially pour rocket fuel on musicians with the most promise. <b>This is the first time to our knowledge that anything like this has been done.</b> To that end, a cornerstone of our mission is to collaborate with like-minded companies, reaching young musicians with high quality products and services and enabling us to host the highest impact event possible.<br/><br/>Our mission is to bring more music to the world. Help us make it happen.</p>
-        </div>
-        <ul className="justify-center h-auto flex-wrap flex mt-2 mb-2 items-center">
+        <div className="flex flex-col items-center justify-center py-4">
+  <p className="max-w-4xl text-center text-audiofeudpurple md:text-xl sm:text-lg text-medium px-4 py-4">
+    For better or for worse, garage jam sessions after school are a thing of the past. As of 2023, more kids than ever are making music on their laptops instead, owing to the massively expanded creative ability designing music literally from the sound wave up introduces. Unfortunately, the vast majority of these bedroom musicians operate alone, lack access to quality equipment, and choose not to share their work. This represents an irrecoverable loss of ideas, creative potential, and dreams. Our goal as an organization is to take a flamethrower to these issues and build community, confidence, and lasting opportunities for the next generation of musicians.
+  </p>
+  <p className="max-w-4xl text-center text-audiofeudpurple md:text-xl sm:text-lg text-medium px-4 py-4">
+    So, we’re launching an overnight in-person event in which high schoolers and eligible college students will network, collaborate, and compete to create an original song from scratch. Artists will present before their peers and a set of qualified judges for the chance to win money and essential sound engineering equipment. The aim of this format is to draw out hidden talent and essentially pour rocket fuel on musicians with the most promise. <b>This is the first time to our knowledge that anything like this has been done.</b> To that end, a cornerstone of our mission is to collaborate with like-minded companies, reaching young musicians with high-quality products and services and enabling us to host the highest impact event possible.
+  </p>
+  <p className="max-w-4xl text-center text-audiofeudpurple md:text-xl sm:text-lg text-medium px-4 py-4">
+    Our mission is to bring more music to the world. Help us make it happen.
+  </p>
+</div>
+        {/*<ul className="justify-center h-auto flex-wrap flex mt-2 mb-2 items-center">
           <div className="flex-col justify-center rounded-full mx-[2vw] mt-10 mb-[50px] h-[300px]">
             <div className="relative h-[200px] w-[250px]">
               <Image
@@ -92,7 +94,7 @@ export default function Home() {
             </div>
             <p className="max-w-[250px] text-center text-audiofeudpurple text-xl">this is a little bit of filler text. stay put for a sexy caption.</p>
           </div>
-        </ul>
+  </ul>*/}
       </section>
       <section id="team" className="relative team min-h-[500px] h-fit bg-audiofeudpurple">
         <div className="custom-shape-divider-top-1692368077">
@@ -157,20 +159,20 @@ export default function Home() {
           <div className="relative tier h-[450px] bg-white rounded-[20px] mx-[2vw] min-w-[280px] w-[280px] my-2 border-2 border-audiofeudpurple drop-shadow-[0_0_0_#4824b3] transition duration-300 ease-in-out hover:drop-shadow-[-10px_10px_0_#4824b3]">
             <ul className="absolute top-[15px] left-[15px] right-[15px]">
               <h1 className="text-audiofeudpurpledark text-2xl font-bold">Bronze</h1>
-              <p className="text-audiofeudpurpledark">$500</p>
+              <p className="text-audiofeudpurpledark">$600</p>
               <br />
               <div className="w-full bg-audiofeudpurple h-[2px] mt-[11.5px]"></div>
               <div className="flex mt-[20px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">This is one line of filler text.</p>
+                <p className="flex-initial">Have your logo on our volunteer t-shirts.</p>
               </div>
               <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Actually let&apos;s make that two lines of filler text.</p>
+                <p className="flex-initial">Display your logo on our website.</p>
               </div>
               <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Let&apos;s see what happens if we turn it into three lines full of filler text.</p>
+                <p className="flex-initial">Give out your company swag.</p>
               </div>
             </ul>
             <div className="absolute bottom-[15px] left-[15px] right-[15px] ">
@@ -180,24 +182,20 @@ export default function Home() {
           <div className="relative tier h-[450px] bg-white rounded-[20px] mx-[2vw] min-w-[280px] w-[280px] my-2 border-2 border-audiofeudpurple drop-shadow-[0_0_0_#4824b3] transition duration-300 ease-in-out hover:drop-shadow-[-10px_10px_0_#4824b3]">
             <ul className="absolute top-[15px] left-[15px] right-[15px]">
               <h1 className="text-audiofeudpurpledark text-2xl font-bold">Silver</h1>
-              <p className="text-audiofeudpurpledark">$1k</p>
+              <p className="text-audiofeudpurpledark">$1.2k</p>
               <p className="text-sm text-audiofeudpurpledark opacity-70">*benefits from previous tier stack</p>
               <div className="w-full bg-audiofeudpurple h-[2px] mt-[15px]"></div>
               <div className="flex mt-[20px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">This is one line of filler text.</p>
+                <p className="flex-initial">Set up a sponsor table or booth at our event.</p>
               </div>
               <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Actually let&apos;s make that two lines of filler text.</p>
+                <p className="flex-initial">Promote and provide special offerings to sell your products or services.</p>
               </div>
               <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Let&apos;s see what happens if we turn it into three lines full of filler text.</p>
-              </div>
-              <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
-                <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Let&apos;s make this a bit variable</p>
+                <p className="flex-initial">Send expert mentors from your company.</p>
               </div>
             </ul>
             <div className="absolute bottom-[15px] left-[15px] right-[15px] ">
@@ -207,24 +205,20 @@ export default function Home() {
           <div className="relative tier h-[450px] bg-white rounded-[20px] mx-[2vw] min-w-[280px] w-[280px] my-2 border-2 border-audiofeudpurple drop-shadow-[0_0_0_#4824b3] transition duration-300 ease-in-out hover:drop-shadow-[-10px_10px_0_#4824b3]">
             <ul className="absolute top-[15px] left-[15px] right-[15px]">
               <h1 className="text-audiofeudpurpledark text-2xl font-bold">Gold</h1>
-              <p className="text-audiofeudpurpledark">$2k</p>
+              <p className="text-audiofeudpurpledark">$3k</p>
               <p className="text-sm text-audiofeudpurpledark opacity-70">*benefits from previous tier stack</p>
               <div className="w-full bg-audiofeudpurple h-[2px] mt-[15px]"></div>
               <div className="flex mt-[20px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">This is one line of filler text.</p>
+                <p className="flex-initial">Give a 5 minute presentation about your product or service.</p>
               </div>
               <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Actually let&apos;s make that two lines of filler text.</p>
+                <p className="flex-initial">A reserved position to appoint a judge of your choice.</p>
               </div>
               <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Let&apos;s see what happens if we turn it into three lines full of filler text.</p>
-              </div>
-              <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
-                <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">What if we had two sections with two lines.</p>
+                <p className="flex-initial">Set up a company-sponsored workshop or side event.</p>
               </div>
             </ul>
             <div className="absolute bottom-[15px] left-[15px] right-[15px] ">
@@ -239,15 +233,15 @@ export default function Home() {
               <div className="w-full bg-audiofeudpurple h-[2px] mt-[15px]"></div>
               <div className="flex mt-[20px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">This is one line of filler text.</p>
+                <p className="flex-initial">Have your logo on our organizer hoodies.</p>
               </div>
               <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Actually let&apos;s make that two lines of filler text.</p>
+                <p className="flex-initial">Give a presentation at our award ceremony.</p>
               </div>
               <div className="flex mt-[5px] mb-[5px] text-audiofeudpurpledark">
                 <svg fill="#34b233" className="flex-none mt-[3px] mr-[10px]" width="18" height="18" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><polygon points="35.5,69.2 12.7,46.5 5.5,53.8 35.5,83.8 95.5,23.8 88.2,16.5 " /></svg>
-                <p className="flex-initial">Let&apos;s see what happens if we turn it into three lines full of filler text.</p>
+                <p className="flex-initial">A custom, company sponsored award with a prize of your choice.</p>
               </div>
             </ul>
             <div className="absolute bottom-[15px] left-[15px] right-[15px] ">
